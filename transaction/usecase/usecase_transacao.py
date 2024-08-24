fa720ae7646b049b026803727ff0ad926ceb276d
Marcelo <marceloschuler1234@gmail.com>
2024-08-24 16:33:14 -0300

    fix: import from account package

--- transaction/usecase/usecase_transacao.py
+++ transaction/usecase/usecase_transacao.py
@@ -1,5 +1,5 @@
 from transaction.application.transacao_application import TransacaoApplication
-from conta.application.conta_application import ContaApplication
+from account.application.conta_application import ContaApplication
 from Infra.utilities.with_db_connection import with_db_connection
 from Infra.utilities.convert_to_dto import ConvertToDTO
 from transaction.dto.transacao_dto import TransacaoDTO
